86a29344514e0cb6fcc2c17ecb8d31be353606dc
MarcoMura85 <marcomura85@gmail.com>
2019-07-17 12:15:23 +0200

    hyperparameters optimization

--- fashionMNIST.py
+++ fashionMNIST.py
@@ -29,58 +29,61 @@ lr_list = [.01, .001, .0001, .00001]
 
 parameters = dict(
     lr=[.01, .001]
-    , batch_size=[100, 1000]
+    , batch_size=[10, 100, 1000]
     , shuffle=[True, False]
 )
+param_values = [v for v in parameters.values()]
 
-network = networkClass.Network()
-torch.backends.cudnn.benchmark=True
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=100)
-optimizer = optim.Adam(network.parameters(), lr=0.01)
+for lr, batch_size, shuffle in product(*param_values):
+    network = networkClass.Network()
+    #torch.backends.cudnn.benchmark=True
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=shuffle)
+    optimizer = optim.Adam(network.parameters(), lr=lr)
 
-images, labels = next(iter(train_loader))
-grid = torchvision.utils.make_grid(images)
+    images, labels = next(iter(train_loader))
+    grid = torchvision.utils.make_grid(images)
 
-tb = SummaryWriter()
-tb.add_image('images', grid)
-tb.add_graph(network, images)
+    comment = f' batch_size={batch_size} lr={lr} shuffle={shuffle}'
+    tb = SummaryWriter(comment=comment)
+    tb.add_image('images', grid)
+    tb.add_graph(network, images)
 
-for epoch in range(10):
+    for epoch in range(10):
 
-    print("starting epoch:", epoch)
+        print("starting epoch:", epoch)
 
-    total_loss = 0
-    total_correct = 0
+        total_loss = 0
+        total_correct = 0
 
-    for batch in train_loader:  # Get Batch
-        images, labels = batch
+        for batch in train_loader:  # Get Batch
+            images, labels = batch
 
-        preds = network(images)  # Pass Batch
-        loss = F.cross_entropy(preds, labels)  # Calculate Loss
+            preds = network(images)  # Pass Batch
+            loss = F.cross_entropy(preds, labels)  # Calculate Loss
 
-        #print('loss:', loss.item())
-        #print(get_num_correct(preds, labels))
+            #print('loss:', loss.item())
+            #print(get_num_correct(preds, labels))
 
-        optimizer.zero_grad()
-        loss.backward()  # Calculate Gradients
-        optimizer.step()  # Update Weights
+            optimizer.zero_grad()
+            loss.backward()  # Calculate Gradients
+            optimizer.step()  # Update Weights
 
-        total_loss += loss.item()
-        total_correct += get_num_correct(preds, labels)
+            total_loss += loss.item() * batch_size
+            total_correct += get_num_correct(preds, labels)
 
-    tb.add_scalar('Loss', total_loss, epoch)
-    tb.add_scalar('Number Correct', total_correct, epoch)
-    tb.add_scalar('Accuracy', total_correct / len(train_set), epoch)
+        tb.add_scalar('Loss', total_loss, epoch)
+        tb.add_scalar('Number Correct', total_correct, epoch)
+        tb.add_scalar('Accuracy', total_correct / len(train_set), epoch)
 
-    for name, param in network.named_parameters():
-        tb.add_histogram(name, param, epoch)
-        tb.add_histogram(f'{name}.grad', param.grad, epoch)
+        for name, param in network.named_parameters():
+            tb.add_histogram(name, param, epoch)
+            tb.add_histogram(f'{name}.grad', param.grad, epoch)
 
-    print(
-        "epoch:", epoch,
-        "total_correct:", total_correct,
-        "loss:", total_loss
-    )
+        print(
+            "epoch:", epoch,
+            "total_correct:", total_correct,
+            "loss:", total_loss
+        )
 
-print(total_correct / len(train_set))
+    print(total_correct / len(train_set))
 tb.close()
